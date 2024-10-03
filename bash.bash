c7d4cb3c35221c786d206aa51c4b235d57e56383
Spect COW <12322543-Spcfork@users.noreply.replit.com>
2024-10-03 03:00:17 +0000

    first commit

--- bash.bash
+++ bash.bash
@@ -1,5 +1,6 @@
-oak build --entry src/main.oak --output dist/soak.oak
-oak build --entry src/main.oak --output dist/soak.js --web
+SOAK_INCLUDES="std,str,math,sort,random,fs,fmt,json,datetime,path,http,test,debug,cli,md,crypto,syntax"
+oak build --entry src/main.oak --output dist/soak.oak --include $SOAK_INCLUDES
+oak build --entry src/main.oak --output dist/soak.js --web --include $SOAK_INCLUDES
 
-oak pack --entry src/main.oak --output dist/soak
+oak pack --entry src/main.oak --output dist/soak --include $SOAK_INCLUDES
 chmod +x dist/soak
